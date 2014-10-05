2c84e536548481d1b908b5826dd3345843f88fe0
Vadim Lavorchuk <lavorchukvadim@gmail.com>
2014-10-05 17:02:15 +0300

    some labs

---
+++ ООП/PrW1/WindowsFormsApplication1/WindowsFormsApplication1/Class1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication1
+{
+    class TimeFun
+    {
+        static public int getHour(Time time)
+        {
+            return time.hours;
+        }
+
+        static public int getMinutes(Time time)
+        {
+            return time.minutes;
+        }
+
+        static public int getSeconds(Time time)
+        {
+            return time.seconds;
+        }
+
+        static public void set(int h, int m, int s, Time time)
+        {
+            if ((h >= 0 && h < 24) && (m >= 0 && m < 60) && (s >= 0 && s < 60))
+            {
+                time.hours = h;
+                time.minutes = m;
+                time.seconds = s;
+            }
+            else
+            {
+                time.hours = 23;
+                time.minutes = 59;
+                time.seconds = 59;
+            }
+        }
+
+        static public void input(Time time)
+        {
+            try
+            {
+                Console.Write("input hours=");
+                int h = Convert.ToInt32(Console.ReadLine());
+                Console.Write("input minutes=");
+                int m = Convert.ToInt32(Console.ReadLine());
+                Console.Write("input seconds=");
+                int s = Convert.ToInt32(Console.ReadLine());
+                set(h, m, s, time);
+            }
+            catch
+            {
+                Console.WriteLine("input error");
+            }
+        }
+
+        static public void output(Time time)
+        {
+            Console.WriteLine("Time: " + time.hours + ":" + time.minutes + ":" + time.seconds);
+        }
+
+        static public void addHours(Time time)
+        {
+            if (time.hours > 22)
+                time.hours = 0;
+            else
+                time.hours++;
+        }
+
+        static public void minHours(Time time)
+        {
+            if (time.hours < 1)
+                time.hours = 23;
+            else
+                time.hours--;
+        }
+
+        static public void addMinutes(Time time)
+        {
+            if (time.minutes > 58)
+            {
+                time.addHours();
+                time.minutes = 0;
+            }
+            else time.minutes++;
+        }
+
+        static public void minMinutes(Time time)
+        {
+            if (time.minutes < 1)
+            {
+                time.minHours();
+                time.minutes = 59;
+            }
+            else time.minutes--;
+        }
+
+        static public void addSeconds(Time time)
+        {
+            if (time.seconds > 58)
+            {
+                time.addMinutes();
+                time.seconds = 0;
+            }
+            else time.seconds++;
+        }
+
+        static public void minSeconds(Time time)
+        {
+            if (time.seconds < 1)
+            {
+                time.minMinutes();
+                time.seconds = 59;
+            }
+            else time.seconds--;
+        }
+
+        static public void initialize(Time time)
+        {
+            time.hours = 0;
+            time.minutes = 0;
+            time.seconds = 0;
+        }
+    }
+}
---
+++ ООП/PrW1/WindowsFormsApplication1/WindowsFormsApplication1/Class2.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication1
+{
+    class Class2
+    {
+        public struct Time
+        {
+            public int hours, minutes, seconds;
+        }
+            public int getHour(Time t)
+            {
+                return t.hours;
+            }
+
+            public int getMinutes(Time t)
+            {
+                return t.minutes;
+            }
+
+            public int getSeconds(Time t)
+            {
+                return t.seconds;
+            }
+
+            public Time set(int h, int m, int s, Time t)
+            {
+                if ((h >= 0 && h < 24) && (m >= 0 && m < 60) && (s >= 0 && s < 60))
+                {
+                    t.hours = h;
+                    t.minutes = m;
+                    t.seconds = s;
+                }
+                else
+                {
+                    t.hours = 23;
+                    t.minutes = 59;
+                    t.seconds = 59;
+                }
+                return t;
+            }
+
+            public void input(Time t)
+            {
+                try
+                {
+                    Console.Write("input hours=");
+                    int h = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("input minutes=");
+                    int m = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("input seconds=");
+                    int s = Convert.ToInt32(Console.ReadLine());
+                    t=set(h, m, s, t);
+                }
+                catch
+                {
+                    Console.WriteLine("input error");
+                }
+            }
+
+            public void output(Time t)
+            {
+                Console.WriteLine("Time: " + t.hours + ":" + t.minutes + ":" + t.seconds);
+            }
+
+            public Time addHours(Time t)
+            {
+                if (t.hours > 22)
+                    t.hours = 0;
+                else
+                    t.hours++;
+                return t;
+            }
+
+            public Time minHours(Time t)
+            {
+                if (t.hours < 1)
+                    t.hours = 23;
+                else
+                    t.hours--;
+                return t;
+            }
+
+            public Time addMinutes(Time t)
+            {
+                if (t.minutes > 58)
+                {
+                   t = addHours(t);
+                    t.minutes = 0;
+                }
+                else t.minutes++;
+                return t;
+            }
+
+            public Time minMinutes(Time t)
+            {
+                if (t.minutes < 1)
+                {
+                    t = minHours(t);
+                    t.minutes = 59;
+                }
+                else t.minutes--;
+                return t;
+            }
+
+            public Time addSeconds(Time t)
+            {
+                if (t.seconds > 58)
+                {
+                    t = addMinutes(t);
+                    t.seconds = 0;
+                }
+                else t.seconds++;
+                return t;
+            }
+
+            public Time minSeconds(Time t)
+            {
+                if (t.seconds < 1)
+                {
+                    t = minMinutes(t);
+                    t.seconds = 59;
+                }
+                else t.seconds--;
+                return t;
+            }
+
+            public Time initialize(Time t)
+            {
+                t.hours = 0;
+                t.minutes = 0;
+                t.seconds = 0;
+                return t;
+            }
+        }
+}
