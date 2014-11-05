9bcb6cbb889f2372bcd40b4970c545daf6469b28
Uspaun <spaun_volkivsky@mail.ru>
2014-11-05 21:48:09 +0200

    lol

--- TakeAQueue/Server/upload.php
+++ TakeAQueue/Server/upload.php
@@ -7,12 +7,40 @@ mysql_select_db("uspaun_pp_ua_project");
 $file = $_FILES['file'];
 $xml = simplexml_load_file($file["tmp_name"]);
 
+$fileIMG = $_FILES['uploadfile'];
+	//if ($fileIMG['type'] != "image/png")
+		//exit;
+$uploaddir = './images/';
+$fileIMG['name']=randstr().'.png';
+$uploadfile = $uploaddir.basename($fileIMG['name']);		
+echo copy($fileIMG['tmp_name'], $uploadfile) ? " image is uploaded" : " false";
 foreach($xml->children() as $people) {  
-	$query = "insert into uspupl (Name) value ('". $people->name ."')";
+	$query = "insert into uspupl (Name, Image) values ('". $people->name ."', 'http://uspaun.pp.ua". $uploadfile ."')";
 	
 	$result = mysql_query($query);
 	if (!$result){
 		exit(mysql_error());
 	}
 }
+echo "xml uploaded and data is inserted";
+	
+	function randstr($len=5)
+	{
+		$arr=array("A-Z","a-z","0-9");
+		unset($simb);
+		for($i=0;$i<count($arr);$i++)
+		{
+			@list($st,$end)=split("-",$arr[$i]);
+			for($j=ord($st);$j<ord($end);$j++)
+			{
+				$simb[]=chr($j);
+			}
+		}
+		$str="";
+		for($i=0;$i<$len;$i++)
+		{
+			$str.=$simb[rand(0,(count($simb)-1))];
+		}
+		return $str;
+	}
 ?>
