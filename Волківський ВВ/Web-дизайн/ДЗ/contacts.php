be3311227ca940b7f4997831080a816d5d1dfc1b
Uspaun <spaun_volkivsky@mail.ru>
2014-11-11 20:52:12 +0200

    шось додав

---
+++ Волківський ВВ/Web-дизайн/ДЗ/contacts.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Зворотній зв'язок</title>
+<link href="content.css" rel="stylesheet" type="text/css">
+<link href="menu.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <header>
+    <a href="index.php"><img src="" width="180" height="90" id="logo" display:block;" /></a>NEWS ALL OF THE WORLD
+    <input type="search" placeholder="пошук по сайту" autocomplete="on" display:block;>
+    </header>
+          <div id="menu">
+      <?php include 'menu.php'; ?>
+      </div>
+    <div id="content">
+    <table width="100%" border="0">
+      <tbody>
+        <tr>
+          <td align="center" valign="center">
+          <iframe src="form.html" width="250px" height="250px" border="0px"></iframe></td>
+          <td width="100px"><div id="commercial">Реклама</div></td>
+        </tr>
+      </tbody>
+    </table>
+    </div>
+<footer>&copy; Віталій Волківський</footer>
+</body>
+</html>
---
+++ Волківський ВВ/Web-дизайн/ДЗ/menu.php
@@ -0,0 +1,12 @@
+<ul id="nav1">
+      <li><a href="index.php">Головна</a>
+      <li><a href="category.php">Категорії</a>
+          <ul>
+            <li><a href="category.php">Спорт</a>
+            <li><a href="category.php">Політики</a>
+            <li><a href="category.php">Економіка</a>
+            <li><a href="category.php">Наука</a>
+          </ul>
+      <li><a href="weather.php">Погода</a>
+      <li class="right"><a href="contacts.php">Зворотній зв'язок</a>
+    </ul>
